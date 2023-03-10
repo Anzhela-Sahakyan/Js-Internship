3b331fd156b9320dbcb3cc3507cb323623ae4d52
Anzhela Sahakyan <sahakyananzhela@gmail.com>
2023-03-10 17:47:21 +0400

    feat:added a new file reduce.js

--- reduce.js
+++ reduce.js
@@ -1,11 +1,31 @@
-const arr = [1, 22, 55, 166, 5, 36, 11, 205, 333, 95, 62, 10, 43];
+// const arr = [1, 22, 55, 166, 5, 36, 11, 205, 333, 95, 62, 10, 43];
 
-const sum = arr.reduce((acc, curr) => {
-  if (curr > 18) {
-    return acc + curr;
-  } else {
-    return acc;
-  }
-}, 0);
+// const sum = arr.reduce((acc, curr) => {
+//   if (curr > 18) {
+//     return acc + curr;
+//   } else {
+//     return acc;
+//   }
+// }, 0);
 
-console.log(sum);
+// console.log(sum);
+
+function getAverageAge(array) {
+  let age = array.reduce((acc, curr) => {
+    return acc + curr.age;
+  }, 0);
+  return age / 2;
+}
+const users = [
+  {
+    username: "Yuri Gagarin",
+    lang: "ru",
+    age: 56,
+  },
+  {
+    username: "Nil Armstrong",
+    lang: "ENG",
+    age: 54,
+  },
+];
+console.log(getAverageAge(users));
